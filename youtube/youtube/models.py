58bbeb958e9bfa947bcc8ff46462d84abb9b38a3
KJOH <rudwls@gmail.com>
2021-02-02 16:29:03 +0900

    Edit model.py

--- youtube/youtube/models.py
+++ youtube/youtube/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+
+# 회원 테이블
+class memberTbl(models.Model):
+    pass
+
+
+# 동영상 테이블
+class videoTbl(models.Model):
+    pass
+
+
+# 재생 목록 테이블
+class playlistTbl(models.Model):
+    userid = models.ForeignKey(memberTbl, on_delete=models.CASCADE)
+    videoid = models.ForeignKey(videoTbl, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ""
+
+
+# 시청 기록 테이블
+class historyTbl(models.Model):
+    userid = models.ForeignKey(memberTbl, on_delete=models.CASCADE)
+    videoid = models.ForeignKey(videoTbl, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ""
