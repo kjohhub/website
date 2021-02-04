86c7305f34c53c3bbc15a4bd2122db7b2d934412
KJOH <rudwls@gmail.com>
2021-02-04 19:48:18 +0900

    검색 기능 추가

--- youtube/youtube/urls.py
+++ youtube/youtube/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),      				# /youtube/
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('results', views.results, name='results')
 ]
--- youtube/youtube/views.py
+++ youtube/youtube/views.py
@@ -4,6 +4,11 @@ from youtube.models import memberTbl, videoTbl, playlistTbl, historyTbl
 
 # index
 def index(request):
-    video_list = videoTbl.objects.all()
-    context = {'video_list' : video_list }
-    return render(request, 'youtube/index.html', context)
+    return render(request, 'youtube/index.html', {})
+
+def results(request):
+    search_key = request.GET.get('search_key')
+    if search_key:
+        video_list = videoTbl.objects.all()
+        video_list = video_list.filter(title__icontains=search_key)
+    return render(request, 'youtube/index.html', {'video_list':video_list})
