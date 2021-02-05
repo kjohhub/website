cfa8ed9fb63b58c9097e0c4a70e4cbc82cf86975
Eomjeongmin <jungmin960710@gmail.com>
2021-02-05 16:28:54 +0900

    210205 edit signup

--- youtube/youtube/urls.py
+++ youtube/youtube/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from youtube import views
 from django.contrib.auth import views as auth_views
+from . import views
 
 app_name = 'youtube'
 urlpatterns = [
     path('', views.index, name='index'),      				# /youtube/
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('results', views.results, name='results')
+    path('results', views.results, name='results'),
+    path('signup/', views.signup, name='signup'),
 ]
--- youtube/youtube/views.py
+++ youtube/youtube/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 		#210205 redirect부분 추가
 from youtube.models import memberTbl, videoTbl, playlistTbl, historyTbl
-
+from django.contrib.auth import authenticate, login		#210205추가
+from youtube.forms import UserForm		#210205추가
 
 # index
 def index(request):
@@ -12,3 +13,21 @@ def results(request):
         video_list = videoTbl.objects.all()
         video_list = video_list.filter(title__icontains=search_key)
     return render(request, 'youtube/index.html', {'video_list':video_list})
+
+#210205추가 회원가입페이지
+def signup(request):
+    """
+    계정생성
+    """
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserForm()
+    return render(request, 'youtube/signup.html', {'form': form})
