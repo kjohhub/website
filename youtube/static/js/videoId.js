24dff97476bc721bfe1773e803e8cf9248fb9eca
KJOH <rudwls@gmail.com>
2021-02-14 21:37:48 +0900

    GUI 수정

--- youtube/static/js/videoId.js
+++ youtube/static/js/videoId.js
@@ -1,5 +1,3 @@
-
-
 function proc(videoInfo) {
   $.each(videoInfo, function (index, item) {
     
@@ -13,24 +11,6 @@ function proc(videoInfo) {
     console.log(item);
   });
 }
-
-
-
-/*
-function proc(videoInfo) {
-  $.each(videoInfo, function (index, item) {
-    let str = 
-    "<tr>" +
-      "<td><label><input type='checkbox' name='color' value='blue'></td>" +
-      "<td><input type='text' name='title'>" + item.title + "</td>" + 
-      "<td>" + item.videoId + "</td>" +
-    "</tr>";
-    $("table").append(str);
-    console.log(item);
-  });
-}
-*/
-
 
 $(document).ready(function(){
   $( "#btn" ).click( function() {
@@ -81,13 +61,10 @@ $(document).ready(function(){
       var tr = checkbox.parent().parent().eq(i);
       var td = tr.children();
       
-      
       // td.eq(0)은 체크박스 이므로  td.eq(1)의 값부터 가져온다.
       var title = td.eq(1).find('input[type="text"]').val();
       var videoid = td.eq(2).find('input[type="text"]').val();
       
-      // 가져온 값을 배열에 담는다.
-      //alert(no + "," + userid);
       $.ajax({
         type: "POST",
         url: "/youtube/manage_video/insert/",
@@ -104,30 +81,3 @@ $(document).ready(function(){
     });
   });
 });
-
-
-/*
-$.ajax({
-  type: "GET",
-  dataType: "JSON",
-  url: "https://www.googleapis.com/youtube/v3/playlistItems?playlistId=PLxCfRlqeGfGnXPhPQNG1Ac0fWLS1gYDjZ&part=snippet&maxResults=100&key=AIzaSyCZ2OTX5bKu60-32PRV5m9tUfjiD08L_q0",
-  contentType: "application/json",
-  success : function(jsonData) {
-
-  for (var i = 0; i < jsonData.items.length; i++) {
-    var items = jsonData.items[i];
-    var title = items.snippet.title;
-    var videoId = items.snippet.resourceId.videoId;
-    console.log("title : "+ title);
-    console.log("videoId : "+ videoId);
-    }
-  },
-
-  complete : function(data) {
-  },
-
-  error : function(xhr, status, error) {
-  console.log("유튜브 요청 에러: "+error);
-  }
-});
-*/
