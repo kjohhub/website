95e426e88d2e68fdd3a534e6a50f950b72bd0d05
Eomjeongmin <jungmin960710@gmail.com>
2021-02-15 17:49:06 +0900

    관리자화면 테이블명 두개로 수정

--- youtube/youtube/admin.py
+++ youtube/youtube/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from youtube.models import videoTbl, playlistTbl, playlistItemTbl, historyTbl
 
-admin.site.register(videoTbl)
+@admin.register(videoTbl)
+class videoTblAdmin(admin.ModelAdmin):
+    list_display = ['videoid','title']
+
 admin.site.register(playlistTbl)
 admin.site.register(playlistItemTbl)
 admin.site.register(historyTbl)
